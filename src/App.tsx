49a36805015ce18ff47fb756c6c3c695f082ae3d
KimMH853 <kimmh853@gmail.com>
2023-07-07 14:38:18 +0900

    표시된 점 클릭 시 색 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,66 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-function Line({ startPoint, endPoint }) {
-  const canvasRef = useRef(null);
+const App = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [pointArray, setPointArray] = useState<{ tag: string; x: number; y: number; isClicked: boolean }[]>([
+    { tag: 'A', x: 100, y: 200, isClicked: false },
+    { tag: 'B', x: 300, y: 200, isClicked: false },
+  ]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
+    if (!canvas) return;
+
     const ctx = canvas.getContext('2d');
+    if (!ctx) return;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // 직선 그리기
-    ctx.beginPath();
-    ctx.moveTo(startPoint.x, startPoint.y);
-    ctx.lineTo(endPoint.x, endPoint.y);
-    ctx.stroke();
-  }, [startPoint, endPoint]);
+    pointArray.forEach((point) => {
+      ctx.beginPath();
+      ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
+      ctx.fillStyle = point.isClicked ? 'blue' : 'black';
+      ctx.fill();
+
+      ctx.font = '12px sans-serif';
+      ctx.fillText(point.tag, point.x - 4, point.y - 6);
+    });
+  }, [pointArray]);
+
+  const handleCanvasClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    const updatedPoints = pointArray.map((point) => {
+      const distance = Math.sqrt((point.x - x) ** 2 + (point.y - y) ** 2);
+      const isClicked = distance <= 10;
+      return {
+        ...point,
+        isClicked: point.isClicked || isClicked,
+      };
+    });
+
+    setPointArray(updatedPoints);
+  };
 
   return (
-    <canvas ref={canvasRef} width={400} height={400} />
-  );
-}
-
-function App() {
-  const startPoint = { x: 100, y: 100 };
-  const endPoint = { x: 300, y: 300 };
-
-  return (
-    <div>
-      <Line startPoint={startPoint} endPoint={endPoint} />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="mb-4">
+        1. On a given finite right line (AB) to construct an equilateral triangle.
+      </div>
+      <canvas
+        ref={canvasRef}
+        width={400}
+        height={400}
+        className="border border-black"
+        onClick={handleCanvasClick}
+      />
     </div>
   );
-}
+};
 
 export default App;
