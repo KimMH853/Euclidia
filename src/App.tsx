945842de9c911d8f9f947e9ab547d6328a584623
KimMH853 <kimmh853@gmail.com>
2023-07-27 19:10:19 +0900

    고치기 전

--- src/App.tsx
+++ src/App.tsx
@@ -32,205 +32,186 @@ const App = () => {
   const [isAnswer, setIsAnswer] = useState(false);
   const [isAnswerClicked, setIsAnswerClicked] = useState(false);
 
-  
   //점과 도형 그리기
   useEffect(() => {
-    initDraw();
-  }, []);
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-  const initDraw = () => {
-    // 초기 문제 그려주기(문제와 관련된 점 그려주기)
-  }
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
-  const addPosForDrawing = () => {
-    //
-  }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  const drawLine = ()=> {
-    //
-  }
+    points.forEach((point) => {
+      ctx.beginPath();
+      ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
+      ctx.fillStyle = point.isClicked ? "blue" : "black";
+      ctx.fill();
 
-  const drawCircle = () => {
-    //
-  }
+      ctx.font = "12px sans-serif";
+      ctx.fillText(point.tag, point.x - 4, point.y - 6);
+    });
 
-  const deleteShape = () => {
-    //
-  }
-
-  const handleCanvasClick = () => {
-    // 선택된 툴이 없을 시 return
-
-    // 클릭 된 지점의 좌표의 위치를 점 배열과 비교 하고 거리가 10 이내면, 점을 그리기위한배열에 좌표 추가
-
-    // 조건1. 선택된 툴이 직선일 때
-
-    // 추가된 좌표가 2개라면, 2개 좌표간의 거리를 구해서 직선 그려주기
+    shapes.forEach((shape) => {
+      if (shape.type === "line") {
+        ctx.beginPath();
+        ctx.moveTo(shape.startX, shape.startY);
+        ctx.lineTo(shape.endX, shape.endY);
+        ctx.strokeStyle = shape.selected ? "red" : "black";
+        ctx.stroke();
+      } else if (shape.type === "circle") {
+        const radius = Math.sqrt(
+          (shape.endX - shape.startX) ** 2 + (shape.endY - shape.startY) ** 2
+        );
+        ctx.beginPath();
+        ctx.arc(shape.startX, shape.startY, radius, 0, 2 * Math.PI);
+        ctx.strokeStyle = shape.selected ? "blue" : "black";
+        ctx.stroke();
+      }
+    });
     
-    // 조건2. 선택된 툴이 원일 때
+    
+  }, [points, shapes]);
 
-    // 추가된 좌표가 2개라면, 2개 좌표간의 거리를 구해서 원을 그려주기
+  const addPoint = ()=>{
+    const circleShapes = shapes.filter((shape) => shape.type === "circle");
+    if (circleShapes.length > prevCircleShapesCount) {
+      const {
+        startX: lastStartX,
+        startY: lastStartY,
+        endX: lastEndX,
+        endY: lastEndY,
+      } = circleShapes[circleShapes.length - 1];
 
-    // 조건3. 선택된 툴이 지우개일 때
+      const lastR1 = distanceBetweenPoints(
+        lastStartX,
+        lastStartY,
+        lastEndX,
+        lastEndY
+      );
 
-    // 직선이나 원이 그려진 좌표를 클릭시 해당 도형을 지움
+      let results: { x: number; y: number }[] = [];
+      results = circleShapes
+        .slice(0, circleShapes.length - 1)
+        .flatMap(({ startX, startY, endX, endY }) => {
+          const r2 = distanceBetweenPoints(startX, startY, endX, endY);
+          console.log(startX);
+          return newPoint(lastStartX, lastStartY, lastR1, startX, startY, r2);
+        });
+
+      const newPoints = results.filter(
+        ({ x, y }) =>
+          !points.some(() => {
+            const comparableX = makeComparableValue(x);
+            const comparableY = makeComparableValue(y);
+            return points.some(
+              (point) =>
+                makeComparableValue(point.x) === comparableX &&
+                makeComparableValue(point.y) === comparableY
+            );
+          })
+      );
+
+      console.log(newPoints);
+
+      setPoints((prevPoints) => [
+        ...prevPoints,
+        ...newPoints.map((point, index) => ({
+          tag: String.fromCharCode(prevPoints.length + index + 65),
+          ...point,
+          isClicked: false,
+        })),
+      ]);
+
+      setPrevCircleShapesCount(circleShapes.length);
+    }
+    
   }
 
-  const handleClickLineButton = () => {
-    // 직선버튼 클릭 -> 선택된 툴 직선으로 변경
+  const addShape = (selectedType: string)=>{
+      const [startPoint, endPoint] = clickedPoints;
+
+      const newShape: Shape = {
+        type: selectedType,
+        startX: startPoint.x,
+        startY: startPoint.y,
+        endX: endPoint.x,
+        endY: endPoint.y,
+        selected: false,
+      };
+
+      setShapes((prevShapes) => [...prevShapes, newShape]);
+      setClickedPoints([]);
+      points.forEach((point) => {
+        point.isClicked = false;
+      });
   }
 
-  const handleClickCircleButton = () => {
-    // 원버튼 클릭 -> 선택된 툴 원으로 변경
-  }
+  const handleCanvasClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+  
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+  
+    const updatedPoints = points.map((point) => {
+      const distance = Math.sqrt((point.x - x) ** 2 + (point.y - y) ** 2);
+      const isClicked = distance <= 10;
 
-  const handleClickEraseButton = () => {
-    // 지우개버튼 클릭 -> 선택된 툴을 지우개로 변경
-  }
+      //clickedPoints 관리
+      if (isClicked) {
+        const duplicationPoints = clickedPoints.filter(
+          (clickedPoint) =>
+            clickedPoint.x === point.x && clickedPoint.y === point.y
+        );
+  
+        if (duplicationPoints.length === 0) {
+          setClickedPoints((prevClicked) => [
+            ...prevClicked,
+            { x: point.x, y: point.y },
+          ]);
+        } else {
+          setClickedPoints((prevClicked) =>
+            prevClicked.filter(
+              (clickedPoint) =>
+                clickedPoint.x !== point.x || clickedPoint.y !== point.y
+            )
+          );
+        }
+      }
+     
+      return {
+        ...point,
+        isClicked: isClicked ? !point.isClicked : point.isClicked,
+      };
+    });
+  
+    setPoints(updatedPoints);
+
+    
+  };
+
+  const handleTypeClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const selectedType = event.currentTarget.name;
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    if(clickedPoints.length ===2) {
+      addShape(selectedType);
+    }
+    addPoint();
+  };
 
   const handleAnswerButtonClick = () => {
-    // 
-  }
+    if (isAnswerClicked && clickedPoints.length === 3) {
+      setIsAnswer(equilateralTriangle(clickedPoints));
+      setClickedPoints([]);
+    }
 
-  // const addPoint = ()=>{
-  //   const circleShapes = shapes.filter((shape) => shape.type === "circle");
-  //   if (circleShapes.length > prevCircleShapesCount) {
-  //     const {
-  //       startX: lastStartX,
-  //       startY: lastStartY,
-  //       endX: lastEndX,
-  //       endY: lastEndY,
-  //     } = circleShapes[circleShapes.length - 1];
-
-  //     const lastR1 = distanceBetweenPoints(
-  //       lastStartX,
-  //       lastStartY,
-  //       lastEndX,
-  //       lastEndY
-  //     );
-
-  //     let results: { x: number; y: number }[] = [];
-  //     results = circleShapes
-  //       .slice(0, circleShapes.length - 1)
-  //       .flatMap(({ startX, startY, endX, endY }) => {
-  //         const r2 = distanceBetweenPoints(startX, startY, endX, endY);
-  //         console.log(startX);
-  //         return newPoint(lastStartX, lastStartY, lastR1, startX, startY, r2);
-  //       });
-
-  //     const newPoints = results.filter(
-  //       ({ x, y }) =>
-  //         !points.some(() => {
-  //           const comparableX = makeComparableValue(x);
-  //           const comparableY = makeComparableValue(y);
-  //           return points.some(
-  //             (point) =>
-  //               makeComparableValue(point.x) === comparableX &&
-  //               makeComparableValue(point.y) === comparableY
-  //           );
-  //         })
-  //     );
-
-  //     console.log(newPoints);
-
-  //     setPoints((prevPoints) => [
-  //       ...prevPoints,
-  //       ...newPoints.map((point, index) => ({
-  //         tag: String.fromCharCode(prevPoints.length + index + 65),
-  //         ...point,
-  //         isClicked: false,
-  //       })),
-  //     ]);
-
-  //     setPrevCircleShapesCount(circleShapes.length);
-  //   }
-    
-  // }
-
-  // const addShape = (selectedType: string)=>{
-  //     const [startPoint, endPoint] = clickedPoints;
-
-  //     const newShape: Shape = {
-  //       type: selectedType,
-  //       startX: startPoint.x,
-  //       startY: startPoint.y,
-  //       endX: endPoint.x,
-  //       endY: endPoint.y,
-  //       selected: false,
-  //     };
-
-  //     setShapes((prevShapes) => [...prevShapes, newShape]);
-  //     setClickedPoints([]);
-  //     points.forEach((point) => {
-  //       point.isClicked = false;
-  //     });
-  // }
-
-  // const handleCanvasClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
-  //   const canvas = canvasRef.current;
-  //   if (!canvas) return;
-  
-  //   const rect = canvas.getBoundingClientRect();
-  //   const x = event.clientX - rect.left;
-  //   const y = event.clientY - rect.top;
-  
-  //   const updatedPoints = points.map((point) => {
-  //     const distance = Math.sqrt((point.x - x) ** 2 + (point.y - y) ** 2);
-  //     const isClicked = distance <= 10;
-
-  //     //clickedPoints 관리
-  //     if (isClicked) {
-  //       const duplicationPoints = clickedPoints.filter(
-  //         (clickedPoint) =>
-  //           clickedPoint.x === point.x && clickedPoint.y === point.y
-  //       );
-  
-  //       if (duplicationPoints.length === 0) {
-  //         setClickedPoints((prevClicked) => [
-  //           ...prevClicked,
-  //           { x: point.x, y: point.y },
-  //         ]);
-  //       } else {
-  //         setClickedPoints((prevClicked) =>
-  //           prevClicked.filter(
-  //             (clickedPoint) =>
-  //               clickedPoint.x !== point.x || clickedPoint.y !== point.y
-  //           )
-  //         );
-  //       }
-  //     }
-     
-  //     return {
-  //       ...point,
-  //       isClicked: isClicked ? !point.isClicked : point.isClicked,
-  //     };
-  //   });
-  
-  //   setPoints(updatedPoints);
-
-    
-  // };
-
-  // const handleTypeClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //   const selectedType = event.currentTarget.name;
-
-  //   const canvas = canvasRef.current;
-  //   if (!canvas) return;
-
-  //   if(clickedPoints.length ===2) {
-  //     addShape(selectedType);
-  //   }
-  //   addPoint();
-  // };
-
-  // const handleAnswerButtonClick = () => {
-  //   if (isAnswerClicked && clickedPoints.length === 3) {
-  //     setIsAnswer(equilateralTriangle(clickedPoints));
-  //     setClickedPoints([]);
-  //   }
-
-  //   setIsAnswerClicked((prev) => !prev);
-  // };
+    setIsAnswerClicked((prev) => !prev);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -246,7 +227,7 @@ const App = () => {
               : "bg-white text-blue-500 border-blue-500 border"
           } rounded cursor-pointer`}
           name="line"
-          onClick={handleClickLineButton}
+          onClick={handleTypeClick}
         >
           직선
         </button>
@@ -258,7 +239,7 @@ const App = () => {
               : "bg-white text-blue-500 border-blue-500 border"
           } rounded cursor-pointer`}
           name="circle"
-          onClick={handleClickCircleButton}
+          onClick={handleTypeClick}
         >
           원
         </button>
